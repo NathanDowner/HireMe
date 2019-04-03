0e482020066ef7df7bb3248c42312840f6de939d
Nathan Downer <nathan.downer@mymona.uwi.edu>
2019-04-03 17:50:30 +0000

    added connection info for database

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "password123"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:8858@localhost/postgres"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://nathan:password123@localhost/hireme"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
